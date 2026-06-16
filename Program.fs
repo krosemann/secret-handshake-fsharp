ca45018b3f41aa1f423558bd09851a37760521c3
Kevin Rosemann <kevin@rosemann-dev.de>
2026-06-16 16:13:19 +0200

    Implement secret handshake

--- Program.fs
+++ Program.fs
@@ -1,1 +1,24 @@
-﻿printfn "Hello world!"
+﻿// This could easily be achieved by using Convert.ToString (number, 2), but I wanted to do it manually for practice
+let isBitSet (number: int) (exponent: int) = (number &&& (1 <<< exponent)) > 0
+
+let actions = [ "wink"; "double blink"; "close your eyes"; "jump" ]
+
+let execute handshake : string list =
+    let isActionPartOfHandshake = [ 0..3 ] |> List.map (isBitSet handshake)
+
+    let actionsInHandshake =
+        [ 0..3 ]
+        |> List.choose (fun x -> if isActionPartOfHandshake[x] then Some actions[x] else None)
+
+    let isReversedOrder = isBitSet handshake 4
+
+    if isReversedOrder then
+        List.rev actionsInHandshake
+    else
+        actionsInHandshake
+
+let printHandshakeAndResult handshake =
+    printfn $"Result for {handshake} is {(execute handshake)}"
+
+printHandshakeAndResult 9
+printHandshakeAndResult 26
