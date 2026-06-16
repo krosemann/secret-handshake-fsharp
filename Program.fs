20f91c269adc0970caf3af685564d1c79427a0e2
Kevin Rosemann <kevin@rosemann-dev.de>
2026-06-16 13:09:34 +0200

    Extract handshake implementation

--- Program.fs
+++ Program.fs
@@ -1,21 +1,6 @@
-﻿// This could easily be achieved by using Convert.ToString (number, 2), but I wanted to do it manually for practice
-let isBitSet (number: int) (exponent: int) = (number &&& (1 <<< exponent)) > 0
+﻿module secret_handshake.Program
 
-let actions = [ "wink"; "double blink"; "close your eyes"; "jump" ]
-
-let execute handshake : string list =
-    let isActionPartOfHandshake = [ 0..3 ] |> List.map (isBitSet handshake)
-
-    let actionsInHandshake =
-        [ 0..3 ]
-        |> List.choose (fun x -> if isActionPartOfHandshake[x] then Some actions[x] else None)
-
-    let isReversedOrder = isBitSet handshake 4
-
-    if isReversedOrder then
-        List.rev actionsInHandshake
-    else
-        actionsInHandshake
+open secret_handshake.SecretHandshake
 
 let printHandshakeAndResult handshake =
     printfn $"Result for {handshake} is {(execute handshake)}"
---
+++ SecretHandshake.fs
@@ -0,0 +1,20 @@
+﻿module secret_handshake.SecretHandshake
+
+let private actions = [ "wink"; "double blink"; "close your eyes"; "jump" ]
+
+// This could easily be achieved by using Convert.ToString (number, 2), but I wanted to do it manually for practice
+let isBitSet (number: int) (exponent: int) = (number &&& (1 <<< exponent)) > 0
+
+let execute handshake : string list =
+    let isActionPartOfHandshake = [ 0..3 ] |> List.map (isBitSet handshake)
+
+    let actionsInHandshake =
+        [ 0..3 ]
+        |> List.choose (fun x -> if isActionPartOfHandshake[x] then Some actions[x] else None)
+
+    let isReversedOrder = isBitSet handshake 4
+
+    if isReversedOrder then
+        List.rev actionsInHandshake
+    else
+        actionsInHandshake
