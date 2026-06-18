b01e1cc5b62b75849e83040fc93f2acc23ba312a
Kevin Rosemann <kevin@rosemann-dev.de>
2026-06-18 11:29:28 +0200

    Use list of tuples to guarantee required key order

--- SecretHandshake.fs
+++ SecretHandshake.fs
@@ -3,21 +3,25 @@
 open secret_handshake.Actions
 
 let private secretActions =
-    Map
-        [ (0b00001, WINK)
-          (0b00010, DOUBLE_BLINK)
-          (0b00100, CLOSE_EYES)
-          (0b01000, JUMP)
-          (0b10000, REVERSAL) ]
+    [ (0b00001, WINK)
+      (0b00010, DOUBLE_BLINK)
+      (0b00100, CLOSE_EYES)
+      (0b01000, JUMP)
+      (0b10000, REVERSAL) ]
 
+let private actionFor bit =
+    List.filter (fun mapping -> fst mapping = bit) secretActions
+    |> List.exactlyOne
+    |> snd
+    
 let private reverseIfNecessary actions =
-    if actions |> List.contains REVERSAL then
+    if List.contains REVERSAL actions then
         List.rev actions |> List.filter (fun x -> x <> REVERSAL)
     else
         actions
 
 let secretHandshake integerCode : string list =
-    Seq.toList secretActions.Keys
+    List.map fst secretActions
     |> List.map (fun bit -> integerCode &&& bit)
-    |> List.choose (fun bit -> if bit > 0 then Some(secretActions[bit]) else None)
+    |> List.choose (fun bit -> if bit > 0 then Some(actionFor bit) else None)
     |> reverseIfNecessary
